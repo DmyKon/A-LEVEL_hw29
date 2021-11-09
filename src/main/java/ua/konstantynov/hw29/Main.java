753b78d494a6b7cb6626672a61c29ab72adbb37b
Dmytro Konstantynov <qdimap1@gmail.com>
2021-11-10 01:34:42 +0200

    ready to deploy in Heroku

--- src/main/java/ua/konstantynov/hw29/Main.java
+++ src/main/java/ua/konstantynov/hw29/Main.java
@@ -8,25 +8,23 @@
 package ua.konstantynov.hw29;
 
 import ua.konstantynov.hw29.entities.Customer;
-import ua.konstantynov.hw29.entities.Order;
 import ua.konstantynov.hw29.service.AlbumService;
 import ua.konstantynov.hw29.service.CustomerService;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
     public static final AlbumService ALBUM_SERVICE = new AlbumService();
     public static final CustomerService CUSTOMER_SERVICE = new CustomerService();
 
     public static void main(String[] args) {
+        System.out.println("<<<<< Start application >>>>>");
         ALBUM_SERVICE.saveRandom(10);
-        List<Order> orderList = new ArrayList<>();
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 3; i++) {
             Customer customer = new Customer("CUSTOMER_NAME_" + i);
+            System.out.println("Saving customer...");
             CUSTOMER_SERVICE.save(customer);
-            orderList.add(CUSTOMER_SERVICE.buyRandomAlbum(customer));
+            System.out.println("Buying album...");
+            System.out.println(CUSTOMER_SERVICE.buyRandomAlbum(customer));
         }
-        orderList.forEach(System.out::println);
+        System.out.println("<<<<< Done! >>>>>");
     }
 }
